00c4a8f9ef27e3712c359d6fd0e13fe08be7b020
Andrii Savchuk <andriksr@gmail.com>
2018-09-19 20:52:56 -0400

    Add basic markup of ToDoListand ToDoSummury components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Notifications from './Notifications';
+import ToDoList from '../todo/ToDoList';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard container">
         <div className="row">
-          <div className="col s12 m6">Lorem ipsum dolor sit amet.</div>
+          <div className="col s12 m6">
+            <ToDoList />
+          </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
           </div>
