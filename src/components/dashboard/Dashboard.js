73956388c8045f181f4f511ac38e94f8b1b4df77
Andrii Savchuk <andriksr@gmail.com>
2018-09-19 20:25:37 -0400

    Add basic markup of Dashboard component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">Lorem ipsum dolor sit amet.</div>
+          <div className="col s12 m5 offset-m1">Lorem ipsum dolor sit.</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
