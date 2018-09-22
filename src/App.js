38a93f7dfbb15e4879adf37d5422f918615c4ad5
Andrii Savchuk <andriksr@gmail.com>
2018-09-21 20:11:37 -0400

    Add content to ToDoDetails component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
+import ToDoDetails from './components/todo/ToDoDetails';
 
 class App extends Component {
   render() {
@@ -10,7 +11,8 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route path="/" component={Dashboard} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/todo/:id" component={ToDoDetails} />
           </Switch>
         </div>
       </Router>
