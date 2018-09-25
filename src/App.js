c2f571d499cb220eab31ccfeb6815bca406bd259
Andrii Savchuk <andriksr@gmail.com>
2018-09-24 20:23:10 -0400

    Add SignUp basic setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ToDoDetails from './components/todo/ToDoDetails';
 import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
             <Route exact path="/" component={Dashboard} />
             <Route path="/todo/:id" component={ToDoDetails} />
             <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </Router>
