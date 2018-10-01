6cc51feae012703484efa1372c1ac033565d07f6
Andrii Savchuk <andriksr@gmail.com>
2018-10-01 19:38:39 -0400

    Add FireBase and config for it

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+import firebas from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyC7jAXTPW1nPcyzYPno-P362rYY2_r6yDw',
+  authDomain: 'todoboard-45eee.firebaseapp.com',
+  databaseURL: 'https://todoboard-45eee.firebaseio.com',
+  projectId: 'todoboard-45eee',
+  storageBucket: 'todoboard-45eee.appspot.com',
+  messagingSenderId: '778232459514'
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
