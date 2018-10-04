e9f5bc9c4020e99be6ca6bd23d30390ee67f6fa5
Andrii Savchuk <andriksr@gmail.com>
2018-10-03 20:16:33 -0400

    Add firebase packages to the project and setup basic configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import  thunk from 'redux-thunk';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import firebaseConfig from './config/firebase';
 
 import './index.css';
 import App from './App';
@@ -10,7 +13,14 @@ import rootReducer from './store/reducers/rootReducer';
 
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/todoActions.js
+++ src/store/actions/todoActions.js
@@ -1,6 +1,6 @@
-export const createTodo = (todo) => {
-  return (dispatch, getState) => {
+export const createTodo = todo => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to DB
-    dispatch({type: 'CREATE_TODO', todo});
-  }
+    dispatch({ type: 'CREATE_TODO', todo });
+  };
 };
