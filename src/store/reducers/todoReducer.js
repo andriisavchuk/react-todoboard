9174c81067c857d31d0098b0cb3d9bcf3589a79d
Andrii Savchuk <andriksr@gmail.com>
2018-10-03 20:50:18 -0400

    Add CREATE_ERROR case for ToDo reducer. Fix typo

--- src/store/reducers/todoReducer.js
+++ src/store/reducers/todoReducer.js
@@ -1,8 +1,8 @@
 const initialState = {
   todos: [
-    {id: '01', title: 'Read book', content: 'Lord of The Rings'},
-    {id: '02', title: 'Watch movie', content: 'IT'},
-    {id: '03', title: 'Learn JS', content: 'Functional JavaScript'}
+    { id: '01', title: 'Read book', content: 'Lord of The Rings' },
+    { id: '02', title: 'Watch movie', content: 'IT' },
+    { id: '03', title: 'Learn JS', content: 'Functional JavaScript' }
   ]
 };
 
@@ -10,8 +10,13 @@ const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'CREATE_TODO':
       console.log('ToDo created', action.todo);
+      return state;
+    case 'CREATE_ERROR':
+      console.log('Create ToDo error', action.err);
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export default todoReducer;
