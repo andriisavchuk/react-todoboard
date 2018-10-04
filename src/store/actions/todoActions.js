b73b3e0a2c6cdb81b871800946597f16d8e72474
Andrii Savchuk <andriksr@gmail.com>
2018-10-03 20:30:51 -0400

    Create firestore add ToDo logic

--- src/store/actions/todoActions.js
+++ src/store/actions/todoActions.js
@@ -1,6 +1,21 @@
 export const createTodo = todo => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to DB
-    dispatch({ type: 'CREATE_TODO', todo });
+    const firestore = getFirestore();
+    firestore
+      .collection('todos')
+      .add({
+        ...todo,
+        firstName: 'Andrii',
+        lastName: 'Sava',
+        userId: 12345,
+        dateStamp: new Date()
+      })
+      .then(() => {
+        dispatch({ type: 'CREATE_TODO', todo });
+      })
+      .catch(err => {
+        dispatch({ type: 'CREATE_ERROR', err });
+      });
   };
 };
